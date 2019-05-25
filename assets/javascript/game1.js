31dd96fe4b9a05718562c33345136061d4336ff1
David Luce <david.luce@outlook.com>
2019-05-25 14:57:25 -0600

    trying to set up javascript

---
+++ assets/javascript/game1.js
@@ -0,0 +1,37 @@
+// var currentWord = ["JavaScript", "CSS", "HTML", "Bootstrap"];
+
+// var youWin = "You Win!!"
+// var alreadyGuessed = ''
+
+// let wins = 0
+
+// let guessesRemaining = 10
+// let guessed = []
+
+var currentWord = ["JavaScript", "CSS", "HTML", "Bootstrap"]
+
+ for(i = 0; i < currentWord.length; i++) {
+    document.getElementById("currentword").innerHTML = (currentWord[i]); 
+ }
+
+
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var wins = 0
+var guesses = 0
+var guessed = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
