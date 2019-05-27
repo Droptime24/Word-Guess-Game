b0e42ce593c51bfe374f4ad8186c4cb8a8515f44
David Luce <david.luce@outlook.com>
2019-05-26 18:40:05 -0600

    updated javascript, started building functions

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -3,10 +3,6 @@
 var possibleWords = [ "JavaScript", "CSS", "HTML", "Bootstrap"];
 console.log(possibleWords);
 
-// Current word on screen
-var currentWord = 'CSS';
-console.log(currentWord);
-
 //  Number of times the word is guessed right
 var totalWins = 0;
 console.log(totalWins);
@@ -23,13 +19,32 @@ console.log(guessesRemaining);
 var guessedLetters = [];
 console.log(guessedLetters);
 
-
-
+// Current word on screen
 var currentWord = possibleWords[Math.floor(Math.random() * possibleWords.length)];
-document.getElementById("#currentWord");
 console.log(currentWord);
 
+function startGame() {
+    for(i =0; i < currentWord.length; i++){
+        var letter = {
+            letter: currentWord[i].toLowerCase(),
+            beenGuessed: false
+        }
+        guessedLetters.push(letter);
+    }   
+    console.log(guessedLetters);
+    toScreen()
+}
 
-
-
-
+// Printing to scren
+function toScreen() {
+    var word = ""
+    for(i = 0; i < guessedLetters.length; i++){
+        if(guessedLetters[i].beenGuessed) {
+            word += guessedLetters[i].letter
+        }else{
+            word += "_ "
+        }
+    }
+    document.getElementById("currentWord").innerHTML = word;
+}
+startGame()
