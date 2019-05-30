16ca8f9fa75237f5ce3756260d4019376e856356
David Luce <david.luce@outlook.com>
2019-05-29 23:09:22 -0600

    What I have so far with JavaScript

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -1,6 +1,6 @@
 
 // Words to be guessed
-var possibleWords = [ "JavaScript", "CSS", "HTML", "Bootstrap"];
+var possibleWords = ["JavaScript", "CSS", "HTML", "Bootstrap"];
 console.log(possibleWords);
 
 //  Number of times the word is guessed right
@@ -19,38 +19,50 @@ console.log(guessesRemaining);
 var guessedLetters = [];
 console.log(guessedLetters);
 
+var guesses = {};
+
 // Current word on screen
 var currentWord = possibleWords[Math.floor(Math.random() * possibleWords.length)];
 console.log(currentWord);
 toScreen()
 
+document.onkeyup = function (event) {
+    var userText = event.key;
+    userText.textContent = "";
+    guesses[userText] = true
+    debugger
+    console.log(userText)
+    toScreen()
+}
+
 // Generate
 function startGame() {
-    for(i =0; i < currentWord.length; i++){
+    for (i = 0; i < currentWord.length; i++) {
         var letter = {
             letter: currentWord[i].toLowerCase(),
             beenGuessed: false
         }
-        guessedLetters.push(letter);
-    }   
-    console.log(guessedLetters);
+        guessedLetters.push(letter).innerHTML = lettersGuessed;
+    }
     toScreen()
 }
 
 // Printing to scren
 function toScreen() {
     var word = ""
-    for(i = 0; i < guessedLetters.length; i++){
-        if(guessedLetters[i].beenGuessed) {
-            word += guessedLetters[i].letter
-        }else{
-            word += "_ " 
+    for (i = 0; i < guessedLetters.length; i++) {
+        debugger
+        if (guesses[guessedLetters[i].letter]) {
+            word += guessedLetters[i].letter;
+        } else {
+            word += "_ "
         }
-        
+
     }
     document.getElementById("currentWord").innerHTML = word;
-    
+    // console.log(word);
 
 }
 startGame()
+
 
